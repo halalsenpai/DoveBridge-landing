a83563557a663657c42891c361ddc35a04508b5e
wasifkhanzada <wasifkhanzada309.wk@gmail.com>
2020-03-24 20:41:49 +0500

    initial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {interceptor} from "./utils/intercepter";
+import {Provider} from "react-redux";
+//global add here
+import './assets/scss/index.scss';
 import * as serviceWorker from './serviceWorker';
+import {store} from "./store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//call axios interceptor
+interceptor();
+
+function Root() {
+    return (
+        <React.StrictMode>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </React.StrictMode>
+    );
+}
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
